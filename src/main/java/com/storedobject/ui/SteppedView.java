14610d2e6d5faf4963f93330ed386de18cabf7d4
Syam <syam@engravgroup.com>
2020-04-08 23:30:55 +0400

    Version updated to 1.5.15 SOCommon version 1.3.6 SOComponents version 4.0.1 Vaadin version 15.0.3 Added new class ui.SteppedView.

---
+++ src/main/java/com/storedobject/ui/SteppedView.java
@@ -0,0 +1,126 @@
+package com.storedobject.ui;
+
+import com.storedobject.vaadin.CSSGrid;
+import com.storedobject.vaadin.View;
+import com.vaadin.flow.component.Component;
+
+/**
+ * Stepped View allows you to create a multi-stepped {@link View}.
+ *
+ * @author Syam
+ */
+public class SteppedView extends View implements Transactional {
+
+    /**
+     * Constructor.
+     *
+     * @param numberOfSteps Number of steps in the view
+     */
+    public SteppedView(int numberOfSteps) {
+        this(null, numberOfSteps);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param caption Caption of the view
+     * @param numberOfSteps Number of steps in the view
+     */
+    public SteppedView(String caption, int numberOfSteps) {
+    }
+
+    /**
+     * Get a label for the step. If you provide a label, that will be displayed along with the step number.
+     *
+     * @param step Step
+     * @return Default implementation returns <code>null</code> so that no label is displayed.
+     */
+    protected String getStepLabel(int step) {
+        return null;
+    }
+
+    /**
+     * Get the component to be used for the respective step. (After creating your component, you may invoke one of these
+     * static methods to position the component if needed: {@link #align(Component, CSSGrid.Position)},
+     * {@link #justify(Component, CSSGrid.Position)}, {@link #center(Component)}).
+     *
+     * @param step Step
+     * @return Component to be added. (Default implementation) returns <code>null</code>.
+     */
+    protected Component getStepComponent(int step) {
+        return null;
+    }
+
+    /**
+     * This method is invoked when you enter into this step.
+     *
+     * @param step Step
+     */
+    protected void enter(int step) {
+    }
+
+    /**
+     * This method is invoked when you go back to the previous step from this step.
+     *
+     * @param step Step
+     */
+    protected void back(int step) {
+    }
+
+    /**
+     * This method is invoked when you complete a step. (From the last step,
+     * you typically do the rest of the processing and {@link #close()} the view.
+     *
+     * @param step Step
+     */
+    protected void complete(int step) {
+    }
+
+    /**
+     * This method is invoked before a forward move happens. If the method doesn't return a <code>true</code> value,
+     * movement will be stopped. So, this can be used to validate the content of the current step.
+     *
+     * @param step Current step
+     * @return Default implementation returns <code>true</code>.
+     */
+    protected boolean isValid(int step) {
+        return true;
+    }
+
+    /**
+     * This method will be invoked when "Cancel" button is pressed. The default behaviour is to {@link #abort()}
+     * the view.
+     */
+    public void cancel() {
+        abort();
+    }
+
+    /**
+     * Justify (horizontally) a component within the view's content area. (You may use this method from within your
+     * {@link #getStepComponent(int)} method to position your component).
+     *
+     * @param component Component
+     * @param position Position
+     */
+    public static void justify(Component component, CSSGrid.Position position) {
+    }
+
+    /**
+     * Align (vertically) a component within the view's content area. (You may use this method from within your
+     * {@link #getStepComponent(int)} method to position your component).
+     *
+     * @param component Component
+     * @param position Position
+     */
+    public static void align(Component component, CSSGrid.Position position) {
+    }
+
+    /**
+     * Center (horizontally and vertically) a component within the view's content area. (You may use this method from within your
+     * {@link #getStepComponent(int)} method to position your component).
+     *
+     * @param component Component
+     */
+    public static void center(Component component) {
+    }
+}
