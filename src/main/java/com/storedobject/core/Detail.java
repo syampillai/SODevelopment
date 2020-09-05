eb081e42d2ceeff5f40247a479edbd5ea00648d9
Syam <syam@engravgroup.com>
2020-09-05 18:07:22 +0400

    Version updated to 3.7.0 SOCommon version 3.2.8 SOComponents version 7.2.1 SOCharts version 0.0.5 Vaadin version 16.0.3 HasContacts interface has a couple of more methods. Detail interface now has default implementation for all methods except one.

--- src/main/java/com/storedobject/core/Detail.java
+++ src/main/java/com/storedobject/core/Detail.java
@@ -1,15 +1,44 @@
 package com.storedobject.core;
 
+/**
+ * The interface to denote that a {@link StoredObject} class is keeping details of a "master"
+ * {@link StoredObject} class.
+ *
+ * @author Syam
+ */
 public interface Detail {
 
+    /**
+     * Get the {@link Id} of the detail class instance.
+     *
+     * @return {@link Id} of the detail class instance.
+     */
     Id getId();
 
+    /**
+     * Get the {@link Id} of the class instance that makes this instance unique. (By default, generally,
+     * the {@link Id} of this instance is returned).
+     *
+     * @return {@link Id} of the class instance that makes this instance unique.
+     */
     default Id getUniqueId() {
         return getId();
     }
 
+    /**
+     * Copy the attribute values from another instance. (This method is no more in use and may be removed
+     * in the future).
+     *
+     * @param detail Another instance from which values to be copied.
+     */
     default void copyValuesFrom(Detail detail) {
     }
 
+    /**
+     * Check if the given class is a "master" fpr this class. (Must return a consistent value).
+     *
+     * @param masterClass Class to check.
+     * @return True if it is a "master" to this detail class. Otherwise, false.
+     */
     boolean isDetailOf(Class<? extends StoredObject> masterClass);
 }
