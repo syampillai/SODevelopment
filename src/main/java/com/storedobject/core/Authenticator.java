157cb1c2d71d6af71317b867cf5da59a0f813c34
Syam <syam@engravgroup.com>
2021-09-28 22:24:01 +0400

    Version updated to 6.6.8
    SOCommon version 4.0.5
    SOComponents version 9.9.9
    SOCharts version 2.3.0
    Vaadin version 21.0.1
    Requires JDK 16.
    New methods in Authenticator.
    Dependency update.

--- src/main/java/com/storedobject/core/Authenticator.java
+++ src/main/java/com/storedobject/core/Authenticator.java
@@ -1,14 +1,94 @@
 package com.storedobject.core;
 
+/**
+ * Interface for defining an external authenticator. If an external authenticator is configured for a particular
+ * "password owner", that authenticator will be used for authenticating that type of "password owners". For example,
+ * to log in to the application, the "password owner" is {@link SystemUser} and if an external authenticator is
+ * configured for {@link SystemUser} class that will be used instead of the built-in one.
+ *
+ * @author Syam
+ */
 public interface Authenticator {
 
+    /**
+     * Authenticate the given password owner.
+     *
+     * @param passwordOwner {@link Id} of the password owner.
+     * @param password Password.
+     * @return True if authenticated successfully.
+     * @throws Exception If any exception occurred. For example, the login is currently blocked.
+     */
     boolean login(Id passwordOwner, char[] password) throws Exception;
 
+    /**
+     * Authenticate the given password owner.
+     *
+     * @param passwordOwner {@link Id} of the password owner.
+     * @param password Password.
+     * @param authenticatorCode Extra authenticator code required - maybe, from a second device. The
+     *                          default implementation ignores this and invokes the {@link #login(Id, char[])} method.
+     * @return True if authenticated successfully.
+     * @throws Exception If any exception occurred. For example, the login is currently blocked.
+     */
     default boolean login(Id passwordOwner, char[] password, int authenticatorCode) throws Exception {
         return login(passwordOwner, password);
     }
 
+    /**
+     * Change the current password to a new one.
+     *
+     * @param passwordOwner {@link Id} of the password owner.
+     * @param currentPassword Current password.
+     * @param newPassword New password
+     * @return True if changed successfully.
+     * @throws Exception If any exception occurred.
+     */
     boolean changePassword(Id passwordOwner, char[] currentPassword, char[] newPassword) throws Exception;
 
+    /**
+     * Reset the password to the initial default password.
+     *
+     * @param passwordOwner {@link Id} of the password owner.
+     * @return True if password reset is successful.
+     * @throws Exception If any exception occurred.
+     */
     boolean resetPassword(Id passwordOwner) throws Exception;
+
+    /**
+     * Set the current transaction manager. This will be invoked before any other method is invoked. The transaction
+     * manager instance passed is not yet verified at this stage but will be verified eventually.
+     * The default implementation doesn't do anything.
+     *
+     * @param tm Transaction manager.
+     */
+    default void setTransactionManager(TransactionManager tm) {
+    }
+
+    /**
+     * This method is invoked when a user is locked because of invalid login attempts.
+     *
+     * @param passwordOwner {@link Id} of the password owner.
+     */
+    default void locked(Id passwordOwner) {
+    }
+
+    /**
+     * This method is invoked when a locked user is unlocked.
+     *
+     * @param passwordOwner {@link Id} of the password owner.
+     */
+    default void unlocked(Id passwordOwner) {
+    }
+
+    /**
+     * This method is invoked when a user fails to log in.
+     *
+     * @param passwordOwner {@link Id} of the password owner. This could be <code>null</code> if no {@link Id} exists
+     *                                for the login name attempted.
+     * @param login Login name tried.
+     * @param ip IP address from which the login was attempted.
+     * @param deviceIdentifier Device identifier.
+     */
+    default void loginFailed(Id passwordOwner, String login, String ip, String deviceIdentifier) {
+    }
 }
--- src/main/java/com/storedobject/ui/Test.java
+++ src/main/java/com/storedobject/ui/Test.java
@@ -1,79 +1,15 @@
 package com.storedobject.ui;
 
-import com.storedobject.pdf.*;
+import com.storedobject.vaadin.DataForm;
 
-public class Test extends PDFReport {
+public class Test extends DataForm {
 
     public Test() {
-        super(Application.get());
+        super("Test");
     }
 
     @Override
-    public void generateContent() {
-
-        PDFTable table; // Variable ot hold the table
-
-        // First table
-        table = new PDFTable(1);
-        table.addCell(createCell("Hello 1 from Table 1"));
-        Text text = new Text(); // Create a text
-        text.newLine(true)
-                .newLine(true) // Force a couple of new lines
-                .append("Hello") // Add some text
-                .append(PDFColor.RED) // Switch the color to red
-                .append(" in red!") // Show something in red
-                .append(14) // Switch the font to 14 pt
-                .append(" Font in 14 pt") // Show something in that font
-                .newLine() // Add a new line
-                .append("Something in blue!", PDFColor.BLUE) // Show something in blue
-                .newLine() // New line again
-                .append(12, PDFFont.BOLD) // Switch to 12 pt bold font
-                .append(PDFColor.BLACK) // Switch to black color
-                .append("I'm in BOLD, but size is 12 pt!") // Some text
-                .newLine()
-                .newLine(true)
-                .newLine(true); // Add 3 new lines, 2 of them are forced
-        table.addCell(createCell(text));
-        table.addCell(createCell("Hello 2 from Table 2"));
-
-        add(table); // Add the table to the page
-
-        // Second table
-        table = new PDFTable(1);
-        table.addCell(createCenteredCell("Hello World")); // Add centered text
-
-        addGap(30); // Add some gap
-
-        add(table); // Add the second table to the page
-
-        // Third table
-        table = new PDFTable(4);
-
-        // Simple cell with no customization
-        PDFCell cell = createCell("Left");
-        table.addCell(cell);
-
-        // Customized cell with no border on the right side
-        cell = createCell("Middle 1", c -> {
-            c.setBorderWidthRight(0);
-            return c;
-        });
-        table.addCell(cell);
-
-        // Customized cell with no border on the left side and centered (horizontally and vertically)
-        cell = createCell("Middle 2", c -> {
-            c.setBorderWidthLeft(0);
-            c.setHorizontalAlignment(PDFElement.ALIGN_CENTER);
-            c.setVerticalAlignment(PDFElement.ALIGN_CENTER);
-            c.setGrayFill(0.9f); // With 90% grey filling
-            return c;
-        });
-        table.addCell(cell);
-
-        // Simple cell with no customization
-        cell = createCell("Right");
-        table.addCell(cell);
-
-        add(table); // Add the third table to the page
+    protected boolean process() {
+        return true;
     }
 }
