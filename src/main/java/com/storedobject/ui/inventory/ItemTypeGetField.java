a5e6956641179d8cbaa7655d5fa8505d900fa274
Syam <syam@engravgroup.com>
2021-12-03 12:47:10 +0400

    Version updated to 7.0.2
    SOCommon version 4.0.9
    SOComponents version 10.0.1
    SOCharts version 2.3.0
    Vaadin version 21.0.8
    Requires JDK 16.
    Dependency update.
    Major code refactoring - part II.

---
+++ src/main/java/com/storedobject/ui/inventory/ItemTypeGetField.java
@@ -0,0 +1,54 @@
+package com.storedobject.ui.inventory;
+
+import com.storedobject.core.InventoryItemType;
+import com.storedobject.core.ObjectIterator;
+import com.storedobject.core.SystemEntity;
+import com.storedobject.ui.ObjectGetField;
+
+public class ItemTypeGetField<T extends InventoryItemType> extends ObjectGetField<T> {
+
+    public ItemTypeGetField(Class<T> objectClass) {
+        this(null, objectClass);
+    }
+
+    public ItemTypeGetField(String label, Class<T> objectClass) {
+        this(label, objectClass, false);
+    }
+
+    public ItemTypeGetField(Class<T> objectClass, boolean allowAny) {
+        this(null, objectClass, allowAny);
+    }
+
+    public ItemTypeGetField(String label, Class<T> objectClass, boolean allowAny) {
+        this(label, objectClass, allowAny, false);
+    }
+
+    public ItemTypeGetField(Class<T> objectClass, boolean allowAny, boolean allowAdd) {
+        this(null, objectClass, allowAny, allowAdd);
+    }
+
+    public ItemTypeGetField(String label, Class<T> objectClass, boolean allowAny, boolean allowAdd) {
+        super(label, objectClass, allowAny, allowAdd);
+    }
+
+    @Override
+    protected GetProvider<T> createGetProvider() {
+        return new GetSupplier<>();
+    }
+
+    private class GetSupplier<O extends T> implements GetProvider<O> {
+
+        @Override
+        public O getTextObject(SystemEntity systemEntity, String searchText) throws Exception {
+            //noinspection unchecked
+            return (O)InventoryItemType.getByPartNumber(getObjectClass(), searchText, isAllowAny());
+        }
+
+        @Override
+        public ObjectIterator<O> listTextObjects(SystemEntity systemEntity, String searchText) throws Exception {
+            //noinspection unchecked
+            return InventoryItemType.listByPartNumber(getObjectClass(), searchText, isAllowAny())
+                    .map(t -> (O)t);
+        }
+    }
+}
