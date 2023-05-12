0a53480d31ff3ad574c52e6e5adf3d501f879553
Syam <syam@engravgroup.com>
2023-05-12 22:38:08 +0400

    Version updated to 8.6.10
    SOCommon version 4.2.6
    SOComponents version 11.1.17
    SOCharts version 3.2.1
    Vaadin version 23.2.16
    Requires JDK 17.
    Dependency update (so-chart)

--- src/main/java/com/storedobject/ui/Test.java
+++ src/main/java/com/storedobject/ui/Test.java
@@ -6,59 +6,63 @@ import com.storedobject.vaadin.CloseableView;
 import com.storedobject.vaadin.View;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Test extends View implements CloseableView {
 
-    // To hold multiple charts
-    private final List<Chart> charts = new ArrayList<>();
-
     public Test() {
-        super("Chart Example 7");
+        super("Sankey Chart");
 
         // Creating a chart display area
         SOChart soChart = new SOChart() {
+
             @Override
             protected String customizeDataJSON(String json, AbstractDataProvider<?> data) throws Exception {
-                System.err.println(json);
+                //System.err.println(json);
                 System.err.println(JSON.create(json).toPrettyString());
                 return super.customizeDataJSON(json, data);
             }
 
             @Override
             protected String customizeJSON(String json) throws Exception {
-                System.err.println(json);
+                //System.err.println(json);
                 System.err.println(JSON.create(json).toPrettyString());
                 return super.customizeJSON(json);
             }
         };
         soChart.setSize("800px", "500px");
 
-        // Create multiple charts
-        createCharts();
+        // Create the chart
+        SankeyChart chart = createChart();
 
-        // Add the chart component(s) to the chart display area
-        charts.forEach(soChart::add);
+        // Add the chart to the display area
+        soChart.add(chart);
 
         // Set the component for the view
         setComponent(new VerticalLayout(soChart));
     }
 
-    private void createCharts() {
-        SankeyDataProvider.Node a = new SankeyDataProvider.Node("A"),
-                b = new SankeyDataProvider.Node("B"),
-                c = new SankeyDataProvider.Node("C"),
-                a1 = new SankeyDataProvider.Node("A1"),
-                a2 = new SankeyDataProvider.Node("A2"),
-                b1 = new SankeyDataProvider.Node("B1");
+    private SankeyChart createChart() {
+        SankeyChart.Node a = new SankeyChart.Node("A"),
+                b = new SankeyChart.Node("B"),
+                c = new SankeyChart.Node("C"),
+                a1 = new SankeyChart.Node("A1"),
+                a2 = new SankeyChart.Node("A2"),
+                b1 = new SankeyChart.Node("B1");
         SankeyData sd = new SankeyData();
-        sd.addEdge(new SankeyDataProvider.Edge(a, a1, 5));
-        sd.addEdge(new SankeyDataProvider.Edge(a, a2, 3));
-        sd.addEdge(new SankeyDataProvider.Edge(b, b1, 8));
-        sd.addEdge(new SankeyDataProvider.Edge(a, b1, 3));
-        sd.addEdge(new SankeyDataProvider.Edge(b1, a1, 1));
-        sd.addEdge(new SankeyDataProvider.Edge(b1, c, 2));
-        charts.add(new SankeyChart(sd));
+        sd.addEdge(new SankeyChart.Edge(a, a1, 5));
+        sd.addEdge(new SankeyChart.Edge(a, a2, 3));
+        sd.addEdge(new SankeyChart.Edge(b, b1, 8));
+        sd.addEdge(new SankeyChart.Edge(a, b1, 3));
+        sd.addEdge(new SankeyChart.Edge(b1, a1, 1));
+        sd.addEdge(new SankeyChart.Edge(b1, c, 2));
+        b1.getItemStyle(true).getBorder(true).setColor(new Color("black"));
+        SankeyChart sc = new SankeyChart(sd);
+        Label label = sc.getLabel(true);
+        label.setRotation(90);
+        label.getTextBorder(true).setWidth(5);
+        label.getBorder(true).setWidth(5);
+        label.getBorder(true).setColor(new Color("red"));
+        ItemStyle is = sc.getItemStyle(true);
+        is.getBorder(true).setColor(new Color("blue"));
+        return sc;
     }
 }
