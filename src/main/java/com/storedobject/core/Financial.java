b74dbc756165c7284a80d05d64b20fccbc45dd33
Syam Pillai <syam.s.pillai@gmail.com>
2024-07-19 20:20:44 +0400

    Version 12.0.0 SOCommon version 4.7.10 SOComponents version 11.1.21 SOCharts version 3.2.5 Vaadin version 23.2.16
    Add HasTax interface and update accounts-related classes
    
    Implemented new HasTax interface in the com.storedobject.core package that represents tax-related behavior in an entity. Several accounts-related classes such as BusinessEntity, AccountEntity, EntityAccount, and PersonalEntity have been updated with new method implementations. The version number has also been increased from 11.2.0 to 12.0.0 in pom.xml.

---
+++ src/main/java/com/storedobject/core/Financial.java
@@ -0,0 +1,75 @@
+package com.storedobject.core;
+
+/**
+ * The Financial interface represents a financial entity that can post ledger transactions.
+ * It provides methods to check if the ledger is already posted, and to post a ledger transaction.
+ * It also provides a default method to post a ledger transaction using a TransactionManager.
+ *
+ * @author Syam
+ */
+public interface Financial {
+
+    /**
+     * Checks if the ledger is already posted.
+     *
+     * @return true if the ledger is already posted, false otherwise.
+     */
+    boolean isLedgerPosted();
+
+    /**
+     * Posts ledger transactions using the provided TransactionManager.
+     *
+     * @param transactionManager the TransactionManager used to post the ledger transaction
+     * @throws Exception if the ledger is already posted
+     */
+    void postLedger(TransactionManager transactionManager) throws Exception;
+
+    static int getCategory(Account account) {
+        return (account.getAccountStatus() >> 5) & 3;
+    }
+
+    static String getCategoryValue(Account account) {
+        return AccountChart.getCategoryValue(getCategory(account));
+    }
+
+    static int getBalanceType(Account account) {
+        return account.getAccountStatus() >> 4;
+    }
+
+    static String getBalanceTypeValue(Account account) {
+        return AccountChart.getBalanceTypeValue(getBalanceType(account));
+    }
+
+    static int getTransactionType(Account account) {
+        int status = (account.getAccountStatus() >> 1) & 3;
+        return status == 0 ? ((account.getAccountStatus() >> 9) & 3) : status;
+    }
+
+    static String getTransactionTypeValue(Account account) {
+        return AccountChart.getTransactionTypeValue(getTransactionType(account));
+    }
+
+    static boolean hasStrictBalanceControl(Account account) {
+        return ((account.getAccountStatus() >> 3) & 1) == 1;
+    }
+
+    static boolean hasLimitCheck(Account account) {
+        return ((account.getAccountStatus() >> 8) & 1) == 1;
+    }
+
+    static boolean isDeepFrozen(Account account) {
+        return ((account.getAccountStatus() >> 7) & 1) == 1;
+    }
+
+    static boolean isSpecial(Account account) {
+        return account instanceof AccountTitle || account instanceof BranchAccount || account instanceof OffsetAccount;
+    }
+
+    static boolean isAsset(Account account) {
+        return getCategory(account) == 0 && getBalanceType(account) == 0;
+    }
+
+    static boolean isLiability(Account account) {
+        return getCategory(account) == 1 && getBalanceType(account) == 1;
+    }
+}
